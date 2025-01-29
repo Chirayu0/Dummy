2fd31e3b604b66afa941f8fd9dde448264999fe7
Chirayu Vyas <c-vyas@mercari.com>
2025-01-29 13:48:39 +0530

    backgroud color added

--- Dummy/ContentView.swift
+++ Dummy/ContentView.swift
@@ -8,14 +8,21 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var backgroundColor = LinearGradient(gradient: Gradient(colors: [.red.opacity(0.7), .black.opacity(0.9)]), startPoint: .top, endPoint: .bottom)
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        
+        ZStack {
+            backgroundColor
+                .ignoresSafeArea()
+            VStack {
+                Image(systemName: "globe")
+                    .imageScale(.large)
+                    .foregroundStyle(.tint)
+                Text("Hello, world!")
+            }
+            .padding()
         }
-        .padding()
     }
 }
 
