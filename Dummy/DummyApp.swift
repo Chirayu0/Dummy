fe8683041bcc5c5428224a129b5252637787ee74
Chirayu Vyas <c-vyas@mercari.com>
2025-01-29 12:33:36 +0530

    first push

---
+++ Dummy/DummyApp.swift
@@ -0,0 +1,17 @@
+//
+//  DummyApp.swift
+//  Dummy
+//
+//  Created by Chirayu Vyas on 29/01/25.
+//
+
+import SwiftUI
+
+@main
+struct DummyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DummyTests/DummyTests.swift
@@ -0,0 +1,17 @@
+//
+//  DummyTests.swift
+//  DummyTests
+//
+//  Created by Chirayu Vyas on 29/01/25.
+//
+
+import Testing
+@testable import Dummy
+
+struct DummyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
